692d3e00714927d7228de13a40fafd4157ee4240
lodhel <lodhelnew@gmail.com>
2019-07-11 22:39:54 +0300

    first commit

---
+++ src/components/Content.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+const Content = () => {
+    return (
+        <div className="content">
+            <img src="https://avatars.mds.yandex.net/get-pdb/906476/cf6e4972-9a6d-465f-b197-fddcd75f5b48/s1200?webp=false" />
+            <div> ava + desc. </div>
+            <div> my post </div>
+            <div> new post </div>
+            <div>post </div>
+        </div>
+    )
+}
+
+export default Content;
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const Header = () => {
+    return (
+        <header className="header">
+            <img src="http://saadbess.github.io/george-bokhua/img/fire.jpg" />
+        </header>
+        )
+}
+
+export default Header;
